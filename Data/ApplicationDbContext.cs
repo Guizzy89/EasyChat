4433f3fcc0bbe6677eb2a5436adf0d932d832226
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-08 19:49:52 +0300

    добавил папку Data, а в ней создал ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EasyChat.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectName.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        // Определяем коллекцию для наших сообщений
+        public DbSet<MyMessage> Messages { get; set; }
+    }
+}
