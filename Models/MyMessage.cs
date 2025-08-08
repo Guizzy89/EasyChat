ebb5d93b2bb49cbcfbf13649022bde7d20717209
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-08 19:33:00 +0300

    Создал сущность MyMessage

---
+++ Models/MyMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyChat.Models
+{
+    public class MyMessage
+    {
+        // Первичный ключ
+        public int Id { get; set; }
+
+        // Само содержимое сообщения
+        [Required]
+        public string Content { get; set; }
+
+        // Время добавления сообщения
+        public DateTime CreatedAt { get; set; }
+    }
+}
