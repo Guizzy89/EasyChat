1794f175a92566ae747db19baaf00b6fd5202dbe
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-08 19:54:30 +0300

    В program.cs добавил подключение к SQLite базе данных и регистрацию компонентов Razor Pages, необходимых для отображения страниц.
    выполнил в PowerShell - dotnet add package Microsoft.EntityFrameworkCore.Sqlite

--- Program.cs
+++ Program.cs
@@ -1,5 +1,37 @@
+using ProjectName.Data;
+using EasyChat.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем контекст базы данных
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=messages.db"));
+
+// Регистрация сервиса Razor Pages
+builder.Services.AddRazorPages();
+
+
 var app = builder.Build();
+
+if (!app.Environment.IsProduction())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapGet("/", () => "Hello World!");
 
